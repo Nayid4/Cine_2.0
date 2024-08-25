89e80db58e8a5cf88bcf0a4156a143a0f06895d0
Nayid04 <nayid2004@gmail.com>
2024-08-25 02:07:12 -0600

    Add cpp of class Movie .h

--- Movie.cpp
+++ Movie.cpp
@@ -1,1 +1,44 @@
 #include "Movie.h"
+
+Movie::Movie(std::string name, int year, int duration, std::string country, std::string review)
+    : name(name), year(year), duration(duration), country(country), review(review) {}
+
+std::string Movie::getName() const {
+    return name;
+}
+
+int Movie::getYear() const {
+    return year;
+}
+
+int Movie::getDuration() const {
+    return duration;
+}
+
+std::string Movie::getCountry() const {
+    return country;
+}
+
+std::string Movie::getReview() const {
+    return review;
+}
+
+void Movie::setName(const std::string& name) {
+    this->name = name;
+}
+
+void Movie::setYear(int year) {
+    this->year = year;
+}
+
+void Movie::setDuration(int duration) {
+    this->duration = duration;
+}
+
+void Movie::setCountry(const std::string& country) {
+    this->country = country;
+}
+
+void Movie::setReview(const std::string& review) {
+    this->review = review;
+}
