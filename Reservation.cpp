fbe1282c7deea460e70eb4438cd22f70158f5a6d
Nayid04 <nayid2004@gmail.com>
2024-08-25 02:11:34 -0600

    Creation of CPP at class Reservation

--- Reservation.cpp
+++ Reservation.cpp
@@ -1,1 +1,36 @@
 #include "Reservation.h"
+
+Reservation::Reservation(Movie movie, TheaterRoom room, Schedule schedule, int bookingNumber)
+    : movie(movie), room(room), schedule(schedule), bookingNumber(bookingNumber) {}
+
+Movie Reservation::getMovie() const {
+    return movie;
+}
+
+TheaterRoom Reservation::getRoom() const {
+    return room;
+}
+
+Schedule Reservation::getSchedule() const {
+    return schedule;
+}
+
+int Reservation::getBookingNumber() const {
+    return bookingNumber;
+}
+
+void Reservation::setMovie(const Movie& movie) {
+    this->movie = movie;
+}
+
+void Reservation::setRoom(const TheaterRoom& room) {
+    this->room = room;
+}
+
+void Reservation::setSchedule(const Schedule& schedule) {
+    this->schedule = schedule;
+}
+
+void Reservation::setBookingNumber(int bookingNumber) {
+    this->bookingNumber = bookingNumber;
+}
