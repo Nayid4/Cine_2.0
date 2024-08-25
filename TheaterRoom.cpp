0cdb4437f2f5eed29393851614e19ded61ba88cf
Nayid04 <nayid2004@gmail.com>
2024-08-25 02:10:00 -0600

    Creation of cpp at class TheaterRoom

--- TheaterRoom.cpp
+++ TheaterRoom.cpp
@@ -1,1 +1,28 @@
 #include "TheaterRoom.h"
+
+TheaterRoom::TheaterRoom(int number, int seatCount, float price)
+    : number(number), seatCount(seatCount), price(price) {}
+
+int TheaterRoom::getNumber() const {
+    return number;
+}
+
+int TheaterRoom::getSeatCount() const {
+    return seatCount;
+}
+
+float TheaterRoom::getPrice() const {
+    return price;
+}
+
+void TheaterRoom::setNumber(int number) {
+    this->number = number;
+}
+
+void TheaterRoom::setSeatCount(int seatCount) {
+    this->seatCount = seatCount;
+}
+
+void TheaterRoom::setPrice(float price) {
+    this->price = price;
+}
