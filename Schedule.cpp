0d331a2ee8fd86d8863d8c2b6bed2b9923dec1d9
Nayid04 <nayid2004@gmail.com>
2024-08-25 02:11:00 -0600

    Creation of CPP at class Schedule

--- Schedule.cpp
+++ Schedule.cpp
@@ -1,1 +1,28 @@
 #include "Schedule.h"
+
+Schedule::Schedule(std::string day, std::string startTime, std::string endTime)
+    : day(day), startTime(startTime), endTime(endTime) {}
+
+std::string Schedule::getDay() const {
+    return day;
+}
+
+std::string Schedule::getStartTime() const {
+    return startTime;
+}
+
+std::string Schedule::getEndTime() const {
+    return endTime;
+}
+
+void Schedule::setDay(const std::string& day) {
+    this->day = day;
+}
+
+void Schedule::setStartTime(const std::string& startTime) {
+    this->startTime = startTime;
+}
+
+void Schedule::setEndTime(const std::string& endTime) {
+    this->endTime = endTime;
+}
